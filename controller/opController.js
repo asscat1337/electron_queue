4e9df73c673c3eb23b48e09837b6128a27b71fe7
Руслан Хазиахметов <sadboys198@gmail.com>
2021-09-26 13:31:38 +0500

    fix session

--- controller/opController.js
+++ controller/opController.js
@@ -3,7 +3,11 @@ const Roles = require('../models/model__test/Roles')
 class opController{
     async renderOp(req,res,next){
         try{
-            res.render('op')
+            if(req.session.userdata.role_id !==Number(req.query.id)){
+                res.status(401).json({'error':'Произошла ошибка'})
+            }else{
+                res.render('op')
+            }
         }catch (e) {
             console.log(e)
             // if(!req.session.userdata) return res.redirect(`/login`)
