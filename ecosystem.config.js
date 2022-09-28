afe679ce3134ddc6c0e1f4f3a17a5aa8e3274cf8
asscat1337 <ruslan.xaziaxmetov@mail.ru>
2022-09-28 11:48:17 +0500

    "Исправлены env точки"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,11 @@ module.exports = {
     script: "./server.js",
     env: {
       NODE_ENV: "development",
+      port:8081
     },
     env_production: {
       NODE_ENV: "production",
+      port:8080
     }
   }]
 }
